fb70344883e1a6881955177b30f5871a4ef7cdeb
EdlenoCMatos <edleno.matos@gmail.com>
2023-07-13 14:05:54 -0300

    meu projeto pet

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 id="app-name">MANDUC&PET</h1>
+      <div id="logo"></div>
+      <p id="welcome-text">Seja bem-vindo!</p>
+      <div id="ad-window">Espaço para propaganda</div>
+      <button id="start-button">Iniciar</button>
+    </div>
+  );
+}
+
+export default App;
